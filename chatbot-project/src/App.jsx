99b6bb36111f72b18daba40fe11abe749a5516e1
Gourish Naik <gourishnaik1477@gmail.com>
2026-03-29 18:12:24 +0530

    minor change

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -11,12 +11,9 @@ function App(){
 
      const [chatMessages , setChatMessages] = useState([]);
    
-    // const [chatMessages , setChatMessages] = array;
-  //  const chatMessages = array[0];
-  //  const setChatMessages = array[1];
+   
     
    return (
-      // <div>  {ChatInput()}  </div>
     <div className="app-container"> 
        {chatMessages.length === 0 && (
           <p className="welcome-message">
